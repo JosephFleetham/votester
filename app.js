3ecc88a68af44546b07f36d55a6bce1a57ad0176
JosephFleetham <joseph.fleetham@gmail.com>
2017-08-02 16:40:27 +0000

    Mapped through Product List

--- app.js
+++ app.js
@@ -1,35 +1,51 @@
 const ProductList = React.createClass({
     render: function () {
+        const products = Data.map((product) => {
+            return (
+                <Product
+                    key={'product-' + product.id}
+                    id={product.id}
+                    title={product.title}
+                    description={product.description}
+                    url={product.url}
+                    votes={product.votes}
+                    submitter_avatar_url={product.submitter_avatar_url}
+                    product_image_url={product.product_image_url}
+                />
+            );
+        });
         return (
             <div className='ui items'>
-                <Product />
+                {products}
             </div>
         );
     },
 });
-
-ReactDOM.render(
-    <ProductList />,
-    document.getElementById('content')
-);
 
 const Product = React.createClass({
     render: function () {
         return (
             <div className='item'>
                 <div className='image'>
-                    <img src='images/products/image-aqua.png' />
+                    <img src={this.props.product_image_url} />
                 </div>
                 <div className='middle aligned content'>
+                    <div className='header'>
+                        <a>
+                            <i className='large caret up icon'></i>
+                        </a>
+                        {this.props.votes}
+                    </div>
                     <div className='description'>
-                        <a>Fort Knight</a>
-                        <p>Authentic renaissance actors, delivered in just 2 weeks.</p>
+                        <a href={this.props.url}>
+                            {this.props.title}
+                        </a>
                     </div>
                     <div className='extra'>
                         <span>Submitted by:</span>
                         <img
                             className='ui avatar image'
-                            src='images/avatars/daniel.jpg'
+                            src={this.props.submitter_avatar_url}
                         />
                     </div>
                 </div>
@@ -38,3 +54,8 @@ const Product = React.createClass({
         );
     },
 });
+
+ReactDOM.render(
+    <ProductList />,
+    document.getElementById('content')
+);
